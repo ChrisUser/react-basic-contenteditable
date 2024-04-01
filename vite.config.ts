cdaf7b85fbb79eec489c3a574bd06127ddd4624a
ChrisUser <cristiano-raimondi@tiscali.it>
2024-04-01 22:39:28 +0200

    feat: add workflow, fix build issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite"
-import { resolve } from "path"
+import { extname, relative, resolve } from "path"
+import { fileURLToPath } from "node:url"
+import glob from "glob"
 import react from "@vitejs/plugin-react-swc"
 import dts from "vite-plugin-dts"
 
@@ -13,6 +15,25 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob.sync("lib/**/*.{ts,tsx}").map((file) => [
+          // 1. The name of the entry point
+          // lib/nested/foo.js becomes nested/foo
+          relative("lib", file.slice(0, file.length - extname(file).length)),
+          // 2. The absolute path to the entry file
+          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+          fileURLToPath(new URL(file, import.meta.url)),
+        ])
+      ),
+      output: {
+        // assetFileNames: '[name][extname]',
+        assetFileNames: (assetInfo) => {
+          const assetName = assetInfo.name?.split(".").at(0)
+          return `${assetName === "style" ? "index" : "[name]"}[extname]`
+        },
+        entryFileNames: "[name].js",
+      },
     },
   },
 })
